356169b3bd0355fc80c512bee3b41ba02afad9e8
parisapourmand <parisa.pourmand@gmail.com>
2023-01-14 13:12:22 +0330

    feat: set projecct id in env

--- rasa_web/urls.py
+++ rasa_web/urls.py
@@ -1,7 +1,7 @@
+from decouple import config
 from django.urls import path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-
+    path('', TemplateView.as_view(template_name='index.html'), {'PROJECT_ID': config("PROJECT_ID")}, name='index'),
 ]

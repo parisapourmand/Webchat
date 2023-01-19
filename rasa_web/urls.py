aa599c80f08cd008a6338bc534ee3ca7a85f8f9c
parisapourmand <parisa.pourmand@gmail.com>
2023-01-19 09:50:44 +0330

    feat: add server root in env

--- rasa_web/urls.py
+++ rasa_web/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), {'PROJECT_ID': config("PROJECT_ID")}, name='index'),
+    path('', TemplateView.as_view(template_name='index.html'),
+         {'PROJECT_ID': config("PROJECT_ID"), 'SERVER_ROOT': config("SERVER_ROOT")}, name='index'),
 ]

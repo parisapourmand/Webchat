a5d365450b7080713e072ee510670b0a37188d18
parisapourmand <parisa.pourmand@gmail.com>
2023-01-20 21:29:37 +0330

    feat: make user inactive until admin confermation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UsernameField, UserCreationForm
 
+
 class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
@@ -12,11 +13,26 @@ class SignUpForm(UserCreationForm):
         for field in ["username", "first_name", "last_name", "password1", "password2"]:
             self.fields[field].help_text = None
 
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.is_active = None
+        if commit:
+            user.save()
+        return user
+
     class Meta:
         model = User
         fields = ("username", "first_name", "last_name", "password1", "password2")
 
+
 class CustomLogInForm(AuthenticationForm):
+    error_messages = {
+        "invalid_login":
+            "رمز یا نام‌کاربری اشتباه است، " +
+            "یا اکانت شما فعال نشده است.",
+        "inactive": "اکانت شما فعال نشده است. تا تایید توسط پشتیبانی شکیبا باشید.",
+    }
+
     def __init__(self, *args, **kwargs):
         super(CustomLogInForm, self).__init__(*args, **kwargs)
         self.fields['username'].label = "نام‌کاربری"
--- webchat/settings.py
+++ webchat/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     config('SERVER_ROOT'),
+    '127.0.0.1'
 ]
 
 
